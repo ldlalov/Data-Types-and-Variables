2f87ed2fb1deae232e61f5aa10e4261c608ae7c5
Любомир Лалов <ldlalov@hotmail.com>
2022-01-23 12:50:51 +0200

    03._Floating_Equality

--- 03. Floating Equality/Program.cs
+++ 03. Floating Equality/Program.cs
@@ -6,7 +6,18 @@ namespace _03._Floating_Equality
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            decimal floatA = decimal.Parse(Console.ReadLine());
+            decimal floatB = decimal.Parse(Console.ReadLine());
+            decimal result = Math.Abs(floatA - floatB);
+            int test = Decimal.Compare(result, 0.000001m);
+            if (test<0)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
         }
     }
 }
---
+++ 04. Refactoring Prime Checker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _04._Refactoring_Prime_Checker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
