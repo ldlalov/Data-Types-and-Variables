16d329e9f45e657c3b3dcb0f9eae3e65b2b965db
Любомир Лалов <ldlalov@hotmail.com>
2022-01-23 23:30:23 +0200

    06._Balanced_Brackets

--- 06. Balanced Brackets/Program.cs
+++ 06. Balanced Brackets/Program.cs
@@ -6,7 +6,34 @@ namespace _06._Balanced_Brackets
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int lines = int.Parse(Console.ReadLine());
+            int startBracket = 0;
+            int endBracket = 0;
+            string input = "";
+            for (int i = 0; i < lines; i++)
+            {
+                input = Console.ReadLine();
+                if (input.ToString() == "(")
+                {
+                    startBracket++;
+                }
+                else if (input.ToString() == ")")
+                {
+                    endBracket++;
+                }
+                if ((endBracket>startBracket)||(startBracket-endBracket>1))
+                {
+                    break;
+                }
+            }
+            if (startBracket== endBracket)
+            {
+                Console.WriteLine("BALANCED");
+            }
+            else
+            {
+                Console.WriteLine("UNBALANCED");
+            }
         }
     }
 }
